0bab57c95a29e621271fe6923b66e95a717de621
Priyankasapp <priyanka.s@applocum.org>
2026-06-01 15:37:06 +0530

    todo functionallity added...!

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-
+import Todo from './components/Todo'
 const App = () => {
   return (
-    <div>
-      adsds
+    <div className='bg-stone-500 grid py-4 min-h-screen'>
+      <Todo />
     </div>
   )
 }
 
-export default App
+export default App;
---
+++ src/components/Todo.jsx
@@ -0,0 +1,56 @@
+import React, { useRef, useState } from 'react'
+import todo_icon from '../assets/todo_icon.png'
+import TodoItems from './TodoItems'
+
+
+const Todo = () => {
+    const [todoList, setTodoList] = useState([]);
+    const inputRef = useRef();
+
+    const add = () => {
+        const inputText = inputRef.current.value.trim();
+        
+        if (inputText === '') {
+            return null;
+        }
+        const newTodo = {
+            id: Date.now(),
+            text: inputText,
+            completed: false
+        }
+        setTodoList((prev) => [...prev, newTodo]);
+        inputRef.current.value = '';
+    }
+
+    const deleteTodo = (id) => {
+        setTodoList((prevTodos)=>{
+            return prevTodos.filter((todo) => todo.id !== id)
+        })
+    }
+  return (
+    <div className='bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl'>
+{/* title  */}
+<div className='flex item-center mt-7 gap-2'>
+    <img src={todo_icon} alt="Todo Icon" className='w-8' />
+    <h1>To-Do List</h1>
+</div>
+
+
+{/* input box  */}
+<div className='flex items-center my-7 bg-gray-200 rounded-full'>
+    <input ref={inputRef} className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600' type="text" placeholder='Add your task here...' />
+    <button onClick={add} className='border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer'>ADD + </button>
+</div>
+
+{/* todo list  */}
+    <div>
+        {todoList.map((item) => (
+            <TodoItems key={item.id} text={item.text} id={item.id} isCompleted={item.completed} deleteTodo={deleteTodo} />
+        ))}
+       
+    </div>
+    </div>
+  )
+}
+
+export default Todo;
